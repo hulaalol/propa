830f838fae8e4a03f0381aa2d7bc9fb63a959e9b
hulaalol <mh252@hdm-stuttgart.de>
2019-12-05 23:15:15 +0100

    changed to JRE 11 for jUnit5

--- exercise4/Task3/src/ConditionTask/ActivityRecorder.java
+++ exercise4/Task3/src/ConditionTask/ActivityRecorder.java
@@ -51,44 +51,4 @@ public class ActivityRecorder {
 		return total;
 
 	}
-
-	public static int activityRecorder2(String[] activities, int year) {
-		int total = 0;
-		for (String activity : activities) {
-			switch (activity) {
-			case "TypeA":
-				total += 7;
-			case "TypeC":
-				total += 6;
-			case "TypeE":
-			case "TypeG":
-			case "TypeH":
-				total += 4;
-			case "TypeJ":
-			case "TypeL":
-				total += 3;
-				break;
-			case "TypeD":
-				total += 0;
-			case "TypeF":
-				total += 1;
-			case "TypeI":
-			case "TypeK":
-				total += 9;
-				break;
-			case "TypeB":
-				if (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0))
-					total += 3;
-				else
-					total += 2;
-				break;
-			default:
-				total = 20;
-				break;
-			}
-		}
-
-		return total;
-	}
-
 }
